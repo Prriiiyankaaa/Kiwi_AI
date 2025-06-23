39d1b391b4c0bd22b3e90bb1705bb5ad018b894b
Prriiiyankaaa <160654284+Prriiiyankaaa@users.noreply.github.com>
2025-06-23 20:17:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,5 @@ def action():
         return jsonify({"status": "failed", "error": str(e)})
 
 if __name__ == "__main__":
-    
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True)
 
